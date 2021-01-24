497d61ce2fada6dcc7c1adfc2b5c2347050c7e57
nattasak <ntsst.1999@gmail.com>
2021-01-24 20:33:55 +0700

    phpsql

---
+++ phpsql/insert_from.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<from action="insert.php" method="post">
+ชื่อ:<input type="text" name="firstname" /> <br/>
+นามสกุล:<input type="text" name="lastname" /> <br/>
+email:<input type="hidden" value="m@mail.com" name="email" /> <br/>
+<input type="submit" value="บันทึก" name="submit_btn" />
+</from>
+</body>
+</html>
