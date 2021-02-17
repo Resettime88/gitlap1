42680cdf503244db268a1bec44a8f47eee7e4b80
Resettime88 <76504709+Resettime88@users.noreply.github.com>
2021-02-17 12:27:46 +0700

    React

---
+++ React/src/Appheader.js
@@ -0,0 +1,12 @@
+function Appheader() {
+    
+    return (
+       
+          <header className="App-header">
+              <img className ="header-logo" src ="/pic/Agumon2.png"/>
+    
+           
+          </header>
+          );
+          }
+    export default Appheader;
---
+++ React/src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"Man",
+      thumbnaiUrl :"/pic/064.jpg"
+      
+    
+    },
+    {
+      title :"Girl",
+      thumbnaiUrl :"/pic/Pikachu.jpg"
+      
+    
+    },
+    {
+      title :"Boy",
+      thumbnaiUrl :"/pic/ocjyqg8bnQPGG7q4s15-o.png"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/AshXYanime.png"
+      
+    
+    }
+  ];
+  export default items;
